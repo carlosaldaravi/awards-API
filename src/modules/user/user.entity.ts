2b0ed9cf39f45181bc8f5fef519627cd40b27944
Carlos <carlosaldaravi11@gmail.com>
2020-09-22 20:31:25 +0200

    [UP] user entity team

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -3,74 +3,67 @@ import {
   Column,
   CreateDateColumn,
   Entity,
-  JoinColumn,
   JoinTable,
   ManyToMany,
-  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
-} from 'typeorm';
-import { RoleType } from './roletype.enum';
-import { Status } from '../../shared/status.enum';
-import { Award } from '../award/award.entity';
-import { Team } from '../team/team.entity';
+} from "typeorm";
+import { RoleType } from "./roletype.enum";
+import { Status } from "../../shared/status.enum";
+import { Award } from "../award/award.entity";
 
-@Entity('users')
+@Entity("users")
 export class User extends BaseEntity {
-  @PrimaryGeneratedColumn('increment')
+  @PrimaryGeneratedColumn("increment")
   id: number;
 
-  @Column({ type: 'varchar', unique: true, length: 20, nullable: false })
+  @Column({ type: "varchar", unique: true, length: 20, nullable: false })
   username: string;
 
-  @Column({ type: 'varchar', nullable: false })
+  @Column({ type: "varchar", nullable: false })
   email: string;
 
-  @Column({ type: 'varchar', nullable: false })
+  @Column({ type: "varchar", nullable: false })
   password: string;
 
-  @Column({ type: 'varchar', length: 25, nullable: true })
+  @Column({ type: "varchar", length: 25, nullable: true })
   firstname: string;
 
-  @Column({ type: 'varchar', length: 50, nullable: true })
+  @Column({ type: "varchar", length: 50, nullable: true })
   lastname: string;
 
-  @Column({ type: 'timestamp', name: 'date_born', nullable: true })
+  @Column({ type: "varchar", length: 20, nullable: true })
+  team: string;
+
+  @Column({ type: "timestamp", name: "date_born", nullable: true })
   dateborn: Date;
 
   @Column({
-    type: 'varchar',
+    type: "varchar",
     length: 10,
     nullable: false,
     default: RoleType.STUDENT,
   })
   role: string;
 
-  @Column({ type: 'varchar', default: Status.ACTIVE, length: 8 })
+  @Column({ type: "varchar", default: Status.ACTIVE, length: 8 })
   status: string;
 
-  @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
+  @CreateDateColumn({ type: "timestamp", name: "created_at" })
   createdAt: Date;
 
-  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at' })
+  @UpdateDateColumn({ type: "timestamp", name: "updated_at" })
   updatedAt: Date;
 
   @ManyToMany(
-    type => Award,
-    award => award.users,
-    { eager: true, cascade: true },
+    (type) => Award,
+    (award) => award.users,
+    { eager: true, cascade: true }
   )
   @JoinTable({
-    name: 'user_awards',
-    joinColumn: { name: 'user_id', referencedColumnName: 'id' },
-    inverseJoinColumn: { name: 'award_id', referencedColumnName: 'id' },
+    name: "user_awards",
+    joinColumn: { name: "user_id", referencedColumnName: "id" },
+    inverseJoinColumn: { name: "award_id", referencedColumnName: "id" },
   })
   awards: Award[];
-
-  @ManyToOne(
-    type => Team,
-    team => team.users,
-  )
-  @JoinColumn({ name: 'team_id' })
-  team: Team;
 }
