9d2ec97d5210e6d5a46ec2e5d1cbab68450d0040
Carlos <carlosaldaravi11@gmail.com>
2020-09-22 20:54:15 +0200

    [ADD] launch fake data when empty database

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { setDefaultValues } from "./database/fake-data/create";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix("api");
+
+  setDefaultValues();
+
   await app.listen(AppModule.port);
-  console.log('Corriendo en el puerto ', AppModule.port);
+  console.log("Corriendo en el puerto ", AppModule.port);
 }
 bootstrap();
